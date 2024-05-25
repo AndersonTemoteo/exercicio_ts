974fc950f15d8640f2c4401aba7eba865e2bbae8
AndersonTemoteo <andersontemoteobezerra@gmail.com>
2024-05-25 18:07:13 -0300

    resolução do exercício

---
+++ src/exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicacao(3, 3));
+const saudacao = (nome) => `Olá, ${nome}!`;
+console.log(saudacao('Anderson'));
---
+++ src/exercicio.ts
@@ -0,0 +1,9 @@
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+console.log(multiplicacao(3,3));
+
+const saudacao = (nome: string): string => `Olá, ${nome}!`;
+
+console.log(saudacao('Anderson'));
